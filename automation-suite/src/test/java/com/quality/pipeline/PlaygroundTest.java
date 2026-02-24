2b150130a271fb13e4a06261869da0821fb264d6
sk1zm0509 <jayantaj0509@gmail.com>
2026-02-23 22:47:11 -0500

    Initial commit for Autonomous QA Pipeline

---
+++ automation-suite/src/test/java/com/quality/pipeline/PlaygroundTest.java
@@ -0,0 +1,43 @@
+package com.quality.pipeline;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PlaygroundTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // CRITICAL for Pipeline
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testLoginFlow() {
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button.radius")).click();
+        
+        boolean isSuccess = driver.findElement(By.id("flash")).getText().contains("You logged into a secure area!");
+        Assert.assertTrue(isSuccess, "Login failed or Success message not found!");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
